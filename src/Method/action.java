863761e834cbe203eec49a90e9f6cc55feeceae8
Admin <Admin@DESKTOP-PBFDFE9>
2023-02-24 10:13:41 -0800

    try once

--- src/Method/action.java
+++ src/Method/action.java
@@ -1,5 +1,22 @@
 package Method;
 
 public class action {
+	 
+	    	public static void main(String[] args) {
+				
+		
+			
+		for(int i=1;i<=5;i++) {
+			for(int j=5;j>=i;j--) {
+				System.out.print(" ");
+			}
+			for(int k=5;k>=i;k--) {
+				System.out.print("*");
+			}
+			System.out.println();	
+		}
+	    	}
+	
 
 }
+// code is completed take a review
